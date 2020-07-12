fd147f098bc7d43d56173aa129a3d1bf55259a60
Ubuntu <ubuntu@ubuntu1604.linuxvmimages.local>
2020-07-12 04:26:21 -0400

    test_slam done and working

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-xterm -e "gazebo " &
-sleep 5
 xterm -e "source /opt/ros/kinetic/setup.bash; roscore" &
-sleep 5
-xterm -e "rosrun rviz rviz"
+sleep 3
+xterm -e "roslaunch my_robot world.launch" &
+sleep 7
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch --screen" &
+sleep 3
+xterm -e "rosrun gmapping slam_gmapping _base_frame:=robot_footprint" &
+sleep 3
+xterm -e "rosrun teleop_twist_keyboard teleop_twist_keyboard.py" 
+sleep 3
